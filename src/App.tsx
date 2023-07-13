42f6016fb8873c68e8d68b85e2a6600e7e31e944
pedro <pedrohrds05@hotmail.com>
2023-07-13 10:50:37 -0300

    feat: adding main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Main from './pages/main/Main';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
